44f45e111a0a0ea84949b94cded7317667848bb6
Wayne Chan <wchan.it@outlook.com>
2021-06-13 04:52:59 -0500

    add cors

---
+++ src/main/java/com/wchan/bookstore/config/CrossOriginConfig.java
@@ -0,0 +1,13 @@
+package com.wchan.bookstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossOriginConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
